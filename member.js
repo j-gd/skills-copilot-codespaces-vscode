db16d7e402a607cd16b0469b32ba125d8aa19498
J-G Demathieu <jdemathieu@gmail.com>
2023-12-18 18:28:32 +0000

    copilot second commit

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillsMember() {
+  var skills = ['HTML', 'CSS', 'JS', 'React', 'Node', 'Python', 'Django'];
+  var member = {
+    name: 'John',
+    age: 20,
+    skills: skills
+  };
+  console.log(member.skills[4]);
+}
